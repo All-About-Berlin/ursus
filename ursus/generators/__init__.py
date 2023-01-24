254ffd26ba2d422eae8056507c92164621fe1869
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-01-24 17:25:01 +0100

    Combine multiple file change event into a single rebuild

--- ursus/generators/__init__.py
+++ ursus/generators/__init__.py
@@ -7,55 +7,31 @@ import threading
 logger = logging.getLogger(__name__)
 
 
-def debounce(wait_time):
-    """
-    Decorator that will debounce a function so that it is called after wait_time seconds
-    If it is called multiple times, will wait for the last call to be debounced and run only this one.
-    """
-    def decorator(function):
-        def debounced(*args, **kwargs):
-            def call_function():
-                debounced._timer = None
-                return function(*args, **kwargs)
-            # if we already have a call to the function currently waiting to be executed, reset the timer
-            if debounced._timer is not None:
-                debounced._timer.cancel()
-
-            # after wait_time, call the function provided to the decorator with its arguments
-            debounced._timer = threading.Timer(wait_time, call_function)
-            debounced._timer.start()
-
-        debounced._timer = None
-        return debounced
-
-    return decorator
-
-
 class GeneratorObserverEventHandler(FileSystemEventHandler):
     def __init__(self, generator: 'Generator', **kwargs):
         self.generator = generator
 
-        self.queued_changes = None
+        self.queued_events = set()
         self.debounce_timer = None
 
         return super().__init__(**kwargs)
 
     def dispatch(self, event):
         if event.event_type in ('created', 'modified', 'moved', 'deleted'):
-            self.on_file_change(event)
+            logger.info(f"File {event.event_type}: {event.src_path}")
 
-    def on_file_change(self, event):
-        if self.debounce_timer:
-            self.debounce_timer.cancel()
+            if self.debounce_timer:
+                self.debounce_timer.cancel()
 
-        self.debounce_timer = threading.Timer(wait_time, call_function)
-        # queue event
-        # start or reset timer
-        # at end of timer, 
-        self.generator.on_file_changes(event.event_type, Path(event.src_path))
+            self.queued_events.add(event)
+            self.debounce_timer = threading.Timer(0.5, self.on_file_changes)
+            self.debounce_timer.start()
 
     def on_file_changes(self):
-        self.on_file_changes(events)
+        changed_files = [Path(event.src_path) for event in self.queued_events]
+        changed_files.extend([Path(e.dest_path) for e in self.queued_events if e.event_type == 'moved'])
+        self.queued_events.clear()
+        self.generator.on_file_changes(changed_files)
 
 
 class Generator:
@@ -72,7 +48,5 @@ class Generator:
     def get_observer_event_handler(self):
         return GeneratorObserverEventHandler(generator=self)
 
-    def on_file_changes(self, change_type, file_path: str):
-        if file_path.is_file():
-            logger.info("File changed: %s", file_path)
-            self.generate(changed_files=[file_path])
+    def on_file_changes(self, changed_files):
+        self.generate(changed_files=changed_files)
