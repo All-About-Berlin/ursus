3cd63c553faa29454d8f9d42ac2b0c8864fb1c72
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-01-29 17:47:02 +0100

    Prevent multiple parallel builds

--- ursus/generators/__init__.py
+++ ursus/generators/__init__.py
@@ -14,24 +14,33 @@ class GeneratorObserverEventHandler(FileSystemEventHandler):
         self.queued_events = set()
         self.debounce_timer = None
 
+        self.is_rebuilding = False
+
         return super().__init__(**kwargs)
+
+    def reschedule_rebuild(self):
+        if self.debounce_timer:
+            self.debounce_timer.cancel()
+        self.debounce_timer = threading.Timer(0.5, self.on_file_changes)
+        self.debounce_timer.start()
 
     def dispatch(self, event):
         if event.event_type in ('created', 'modified', 'moved', 'deleted'):
             logger.info(f"File {event.event_type}: {event.src_path}")
-
-            if self.debounce_timer:
-                self.debounce_timer.cancel()
-
             self.queued_events.add(event)
-            self.debounce_timer = threading.Timer(0.5, self.on_file_changes)
-            self.debounce_timer.start()
+            self.reschedule_rebuild()
 
     def on_file_changes(self):
+        if self.is_rebuilding:
+            self.reschedule_rebuild()
+            return
+
+        self.is_rebuilding = True
         changed_files = set(Path(event.src_path) for event in self.queued_events)
         changed_files.update([Path(e.dest_path) for e in self.queued_events if e.event_type == 'moved'])
         self.queued_events.clear()
         self.generator.on_file_changes(changed_files)
+        self.is_rebuilding = False
 
 
 class Generator:
