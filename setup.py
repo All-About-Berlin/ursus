136ad4f0c7349299f42f7431f2ee579da672a585
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-03-21 10:54:21 +0100

    Update readme

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name='ursus_ssg',
-    version='1.0',
+    version='1.0.1',
     description='Static site generator',
     long_description=long_description,
     long_description_content_type='text/markdown',
