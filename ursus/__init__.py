3e35455f5486daab9ee669544c85328acf18fd7a
Nicolas Bouliane <contact@nicolasbouliane.com>
2023-04-25 12:17:08 +0200

    Make ursus callable as a standalone project

---
+++ ursus/__init__.py
@@ -0,0 +1,44 @@
+from ursus.utils import import_class, get_files_in_path, log_colors
+from ursus.config import config
+from watchdog.observers import Observer
+import logging
+import time
+
+
+def build(watch_for_changes: bool = False):
+    """Runs ursus and builds a static website
+
+    Args:
+        watch_for_changes (bool, optional): Keep running, and rebuild when content or templates change
+    """
+    generator = import_class(config.generator)()
+    generator.generate()
+
+    if watch_for_changes:
+        observer = Observer()
+
+        for path in generator.get_watched_paths():
+            observer.schedule(generator.get_observer_event_handler(), path, recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        finally:
+            observer.stop()
+            observer.join()
+
+
+def lint():
+    """Lints the content for errors"""
+    linters = [import_class(linter_path)() for linter_path in config.linters]
+
+    logger = logging.getLogger(__name__)
+
+    for file_path in sorted(get_files_in_path(config.content_path)):
+        logging.info(f"\033[1mLinting {str(file_path)}\033[0m")
+        for linter in linters:
+            for line_no, message, level in linter.lint(file_path):
+                if line_no:
+                    logger.log(level, f"{log_colors[level]}:{line_no}\033[0m {message}")
+                else:
+                    logger.log(level, f"{message}")
