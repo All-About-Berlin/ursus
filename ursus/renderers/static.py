f861839adb449238097bd01146ca24106efddefc
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-04-08 13:32:15 +0800

    Make StaticAssetRenderer easier to reuse for general file copying

--- ursus/renderers/static.py
+++ ursus/renderers/static.py
@@ -7,29 +7,39 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class StaticAssetRenderer(Renderer):
+class StaticFileRenderer(Renderer):
     """
-    Copies static assets in `templates_path` to `output_path`.
+    Copies static files to `output_path`
     """
-    ignored_suffixes = ('.jinja', )
-
-    def get_assets_to_copy(self, changed_files: set = None):
-        return [
-            f for f in get_files_in_path(config.templates_path)
-            if f.suffix.lower() not in self.ignored_suffixes
-        ]
+    def get_files_to_copy(self, changed_files: set = None):
+        # Return list of tuples: (absolute_original_path, destination_relative_to_output_path)
+        return []
 
     def render(self, context: dict, changed_files: set = None) -> set:
         files_to_keep = set()
-        for asset_path in self.get_assets_to_copy():
-            abs_output_path = config.output_path / asset_path
+        for asset_path, rel_output_path in self.get_files_to_copy():
+            abs_output_path = config.output_path / rel_output_path
 
-            if changed_files is None or config.templates_path / asset_path in changed_files:
-                logger.info('Copying asset %s', str(asset_path))
-                copy_file(config.templates_path / asset_path, abs_output_path)
+            if changed_files is None or asset_path in changed_files:
+                logger.info('Copying asset %s', str(rel_output_path))
+                copy_file(asset_path, abs_output_path)
             files_to_keep.add(asset_path)
 
         return files_to_keep
+
+
+class StaticAssetRenderer(StaticFileRenderer):
+    """
+    Copies static assets in `templates_path` to `output_path`
+    """
+    ignored_suffixes = ('.jinja', )
+
+    def get_files_to_copy(self, changed_files: set = None):
+        return [
+            (config.templates_path / f, f)
+            for f in get_files_in_path(config.templates_path)
+            if f.suffix.lower() not in self.ignored_suffixes
+        ]
 
 
 class ArchiveRenderer(Renderer):
