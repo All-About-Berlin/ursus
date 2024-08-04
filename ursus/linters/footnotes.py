01f51165846a6a662cd80da9b6b5e83ab0dabda9
Nicolas Bouliane <contact@nicolasbouliane.com>
2024-08-04 14:13:23 +0200

    Rewrite footnotes linter to return exact error location

--- ursus/linters/footnotes.py
+++ ursus/linters/footnotes.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from ursus.config import config
 from ursus.linters import RegexLinter
 import logging
 import re
@@ -9,23 +10,32 @@ class OrphanFootnotesLinter(RegexLinter):
     Count footnotes; make sure that they all appear at least twice
     """
     file_suffixes = '.md'
-    regex = re.compile(r'.{0,15}(?P<footnote>\[\^(?P<key>\d+)\])(?P<colon>:?).{0,15}')
+    regex = re.compile(r'(?P<footnote>\[\^(?P<id>\d+)\])(?P<colon>:?)')
 
     def lint(self, file_path: Path):
-        self.undefined_footnotes = {}
-        unused_footnotes = list(super().lint(file_path))
-        for key, uses in self.undefined_footnotes.items():
-            for use in uses:
-                yield 0, f"Undefined footnote: {use}", logging.ERROR
-        yield from unused_footnotes
+        if self.file_suffixes and file_path.suffix.lower() not in self.file_suffixes:
+            return
 
-    def handle_match(self, file_path: Path, match: re.Match):
-        if not match['colon']:  # Footnote reference
-            self.undefined_footnotes.setdefault(match['key'], [])
-            self.undefined_footnotes[match['key']].append(match.group(0))
-        else:  # Footnote definition
-            if match['key'] in self.undefined_footnotes:
-                self.undefined_footnotes.pop(match['key'])
-                pass
-            else:
-                yield f"Unused footnote: {match.group(0)}", logging.WARNING
+        footnotes = {}
+
+        with (config.content_path / file_path).open() as file:
+            for line_no, line in enumerate(file.readlines()):
+                for match in self.regex.finditer(line):
+                    footnotes.setdefault(match['id'], [])
+                    footnotes[match['id']].append({
+                        'position': (line_no, *match.span()),
+                        'is_definition': bool(match['colon']),
+                    })
+
+        for footnote_id, occurences in footnotes.items():
+            definitions = [o for o in occurences if o['is_definition']]
+
+            if len(definitions) == 0:
+                for occurence in occurences:
+                    yield occurence['position'], f"Undefined footnote: [^{footnote_id}]", logging.ERROR
+            elif len(definitions) > 1:
+                for definition in definitions:
+                    yield definition['position'], f"Multiple footnote definitions: [^{footnote_id}]", logging.ERROR
+            elif len(definitions) == len(occurences):
+                for occurence in occurences:
+                    yield occurence['position'], f"Unused footnote: [^{footnote_id}]", logging.ERROR
